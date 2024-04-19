dbb8ac971163ac4c3133efa62db80ba9c7761941
tvanlong <longvan1173@gmail.com>
2024-04-19 16:51:01 +0700

    docs: get token, user information from cookies

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,17 +1,8 @@
-export const getAccessTokenFromCookie = () => {
-  const access_token = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith('access_token='))
-    .split('=')[1]
+import Cookies from 'js-cookie'
 
-  return access_token
-}
+export const getAccessTokenFromCookie = () => Cookies.get('access_token')
 
 export const getProfileFromCookie = () => {
-  const profile = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith('profile='))
-    .split('=')[1]
-
-  return JSON.parse(profile)
+  const profile = Cookies.get('user')
+  return profile ? JSON.parse(profile) : {}
 }
