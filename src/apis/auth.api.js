0c6901b6e695f6e6332bcb8769d5e12b968ba165
tvanlong <longvan1173@gmail.com>
2024-05-28 19:41:29 +0700

    refactor: Update signOut API endpoint

--- src/apis/auth.api.js
+++ src/apis/auth.api.js
@@ -2,4 +2,4 @@ import http from '~/utils/http'
 
 export const signIn = (data) => http.post('/api/auth/signin', data)
 
-export const signOut = () => http.post('/api/auth/signout-admin')
+export const signOut = () => http.post('/api/auth/signout')
