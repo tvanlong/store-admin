44e29ec1eaf9fb6d5d43483b18de94ccc7d17d5e
tvanlong <longvan1173@gmail.com>
2024-06-26 21:54:56 +0700

    refactor: Update API endpoint for changing email

--- src/apis/users.api.js
+++ src/apis/users.api.js
@@ -15,7 +15,3 @@ export const deleteStaff = (id) => http.delete(`/api/users/staffs/${id}`)
 export const updateProfile = (id, data) => http.patch(`/api/users/update-profile/${id}`, data)
 
 export const changePassword = (id, data) => http.patch(`/api/users/change-password/${id}`, data)
-
-export const changeEmail = (id, data) => http.post(`/api/users/change-email/${id}`, data)
-
-export const verifyEmail = (id, data) => http.patch(`/api/users/verify-email/${id}`, data)
